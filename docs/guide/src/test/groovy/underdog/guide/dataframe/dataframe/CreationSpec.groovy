782cc73824b00eb33e33d76b9bf7223c75f14050
Mario G. <mario.ggar@gmail.com>
2024-12-09 00:23:18 +0100

    chore: refactor dataframe/series creation specs

--- docs/guide/src/test/groovy/underdog/guide/dataframe/dataframe/CreationSpec.groovy
+++ docs/guide/src/test/groovy/underdog/guide/dataframe/dataframe/CreationSpec.groovy
@@ -1,8 +1,7 @@
-package underdog.guide.dataframe
+package underdog.guide.dataframe.dataframe
 
 import spock.lang.Specification
 import underdog.DataFrame
-import underdog.Series
 import underdog.Underdog
 
 class CreationSpec extends Specification {
@@ -86,40 +85,5 @@ class CreationSpec extends Specification {
         expect:
         colOfMaps2DataFrame.columns == ['name', 'age']
         colOfMaps2DataFrame.size() == 3
-    }
-
-    def "series from collection"() {
-        setup:
-        // tag::series_from_collection[]
-        Series series = [1, 2, 3].toSeries("numbers")
-        // end::series_from_collection[]
-
-        expect:
-        series.size() == 3
-    }
-
-    def "series from transformation"() {
-        setup:
-        // tag::series_as_transformation_sample[]
-        def numbers = Underdog.df().from([numbers: 1..10], "numbers")
-        // end::series_as_transformation_sample[]
-        println numbers
-
-        when:
-        // tag::series_as_transformation_creation[]
-        numbers['by_two'] = numbers['numbers'] * 2
-        // end::series_as_transformation_creation[]
-        println numbers
-
-        and:
-        // tag::series_from_value_creation[]
-        numbers['one'] = 1
-        // end::series_from_value_creation[]
-        println numbers
-
-        then:
-        numbers.columns == ['numbers', 'by_two', 'one']
-        numbers['by_two'].toList() == (1..10).collect { it * 2d }
-        numbers['one'].toList() == (1..10).collect { 1 }
     }
 }
---
+++ docs/guide/src/test/groovy/underdog/guide/dataframe/series/CreationSpec.groovy
@@ -0,0 +1,41 @@
+package underdog.guide.dataframe.series
+
+import spock.lang.Specification
+import underdog.Series
+
+class CreationSpec extends Specification {
+    def "series from collection"() {
+        setup:
+        // tag::series_from_collection[]
+        Series series = [1, 2, 3].toSeries("numbers")
+        // end::series_from_collection[]
+
+        expect:
+        series.size() == 3
+    }
+
+    def "series from transformation"() {
+        setup:
+        // tag::series_as_transformation_sample[]
+        def numbers = Underdog.df().from([numbers: 1..10], "numbers")
+        // end::series_as_transformation_sample[]
+        println numbers
+
+        when:
+        // tag::series_as_transformation_creation[]
+        numbers['by_two'] = numbers['numbers'] * 2
+        // end::series_as_transformation_creation[]
+        println numbers
+
+        and:
+        // tag::series_from_value_creation[]
+        numbers['one'] = 1
+        // end::series_from_value_creation[]
+        println numbers
+
+        then:
+        numbers.columns == ['numbers', 'by_two', 'one']
+        numbers['by_two'].toList() == (1..10).collect { it * 2d }
+        numbers['one'].toList() == (1..10).collect { 1 }
+    }
+}
